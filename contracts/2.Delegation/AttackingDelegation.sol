e6ad942ee087557a24b8b9017d9f2d308d9fd6b3
Laurence Davies <ljd.davies@gmail.com>
2022-08-09 15:19:30 +0200

    remove space

--- contracts/2.Delegation/AttackingDelegation.sol
+++ contracts/2.Delegation/AttackingDelegation.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
-import "./Delegation.sol";
 
 contract AttackingDelegation {
     bytes4 private constant PWN_SELECTOR = bytes4(keccak256("pwn()"));
